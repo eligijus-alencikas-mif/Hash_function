4cb335c784ef91fee1d4b63709544bc90cf44fc8
Eligijus <eligijus.alencikas@mif.stud.vu.lt>
2025-10-01 12:43:10 +0300

    This working now

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #include <algorithm>
 #include <random>
 
-// static_cast<char>(((target + increment - 33) % 94)+ 33);
-
 size_t incrementor = 1;
 
 size_t concatenate(size_t a, size_t b) {
@@ -21,72 +19,41 @@ std::string splitChar(char target) {
     }
 
     split += static_cast<char>((target + ++incrementor - 33)*3 % 94 + 33);
-    split += static_cast<char>((split.at(0) + ++incrementor - 33)*3 % 94+ 33);
+    split += static_cast<char>((split.at(0) + ++incrementor - 33)*3 % 94 + 33);
     return split;
 }
 
-// char combineChars(neChars(std::string split) {
-//     size_t l1_pos = find_letter_pos(split.at(0), charOrder) + 1;
-//     size_t l2_pos = find_letter_pos(split.at(1), charOrder) + 1;
-//
-//     auto position = (concatenate(l1_pos, l2_pos)^2)/2;
-//
-//     if (position + 1 > charOrder.size()) {
-//         position -= charOrder.size() * std::floor(position/charOrder.size());
-//     }
-//
-//     return charOrder.at(position);
-// }
+char combineChars(const char a, const char b) {
+    const auto position = concatenate(a, b);
+
+    return static_cast<char>((position + ++incrementor - 33)*3 % 94 + 33);
+}
 
 int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Need an input" << std::endl;
+        return 1;
+    }
+    const std::string input = argv[1];
 
-    std::cout << splitChar('a');
-    std::cout << splitChar('a');
-    std::cout << splitChar('a');
+    std::cout << "The input is: " << input << std::endl;
+    std::cout << "Input length: " << input.size() << std::endl;
 
-    // if (argc < 2) {
-    //     std::cerr << "Need an input" << std::endl;
-    //     return 1;
-    // }
-    // const std::string input = argv[1];
-    //
-    // std::cout << "The input is: " << input << std::endl;
-    // std::cout << "Input length: " << input.size() << std::endl;
-    //
-    // std::string hash = input;
-    // // for (const auto letter : input) {
-    // //     hash += splitChar(letter,charOrder);
-    // // }
-    //
-    // {
-    //     std::string comb;
-    //     comb += hash.at(0);
-    //     comb += hash.at(1);
-    //     hash.at(0) = combineChars(comb);
-    // }
-    // for (size_t i = 1; i < input.size(); i++) {
-    //     std::string comb;
-    //     comb += hash.at(i-1);
-    //     comb += hash.at(i);
-    //
-    //     hash.at(i) = combineChars(comb);
-    // }
-    //
-    // for (size_t i = hash.length(); i > 0; i--) {
-    //     std::string comb;
-    //     comb += hash.at(i-1);
-    //
-    //     if (i-1 == 0) {
-    //         comb += hash.at(i);
-    //     }else {
-    //         comb += hash.at(i-2);
-    //     }
-    //
-    //     hash.at(i-1) = combineChars(comb);
-    // }
-    //
-    // std::cout << "The hash: " << hash << std::endl;
-    // std::cout << "Hash length: " << hash.size() << std::endl;
+    std::string hash = input;
+
+
+    hash.at(0) = combineChars(hash.at(0), hash.at(1));
+    for (size_t i = 1; i < input.size(); i++) {
+        hash.at(i) = combineChars(hash.at(i-1), hash.at(i));
+    }
+
+    for (size_t i = input.size() - 2; i > 0; i--) {
+        hash.at(i) = combineChars(hash.at(i+1), hash.at(i));
+    }
+    hash.at(0) = combineChars(hash.at(0), hash.at(1));
+
+    std::cout << "The hash: " << hash << std::endl;
+    std::cout << "Hash length: " << hash.size() << std::endl;
 
     return 0;
 }
